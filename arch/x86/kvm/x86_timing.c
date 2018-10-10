968e2f1d70024c63f4de303d274f061c6b904c10
Mark <markm@cs.wisc.edu>
2018-10-10 10:58:35 -0500

    get rid of printing altogether

--- arch/x86/kvm/x86_timing.c
+++ arch/x86/kvm/x86_timing.c
@@ -7,18 +7,18 @@ static unsigned long long elapsed = 0;
 
 void kvm_x86_elapse_time(unsigned long long extra) {
     elapsed += extra;
-    printk(KERN_DEBUG "elapsed %llu\n", extra);
+    //printk(KERN_DEBUG "elapsed %llu\n", extra);
 }
 EXPORT_SYMBOL(kvm_x86_elapse_time);
 
 void kvm_x86_reset_time(void)
 {
     elapsed = 0;
-    printk(KERN_DEBUG "elapsed reset");
+    //printk(KERN_DEBUG "elapsed reset");
 }
 
 unsigned long long kvm_x86_get_time(void)
 {
-    printk(KERN_DEBUG "get elapsed %llu\n", elapsed);
+    //printk(KERN_DEBUG "get elapsed %llu\n", elapsed);
     return elapsed;
 }
